6035edce478b821a7230bc580f85bbe8fb1941a5
sajadkhavas <144109918+sajadkhavas@users.noreply.github.com>
2026-08-06 10:10:46 +0330

    fix(f3b): avoid native title prop collision in headings

--- src/components/system/section-heading.tsx
+++ src/components/system/section-heading.tsx
@@ -2,7 +2,9 @@ import * as React from "react";
 
 import { cn } from "@/lib/utils";
 
-export interface SectionHeadingProps extends React.HTMLAttributes<HTMLDivElement> {
+type SectionHeadingBaseProps = Omit<React.HTMLAttributes<HTMLDivElement>, "title">;
+
+export interface SectionHeadingProps extends SectionHeadingBaseProps {
   as?: "h2" | "h3";
   eyebrow?: React.ReactNode;
   title: React.ReactNode;
